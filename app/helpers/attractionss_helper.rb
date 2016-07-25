d2db3c92b8767da17ebde896c7322bfdeb1bede4
mcasciato <michael.casciato@icloud.com>
2016-07-25 13:06:38 -0400

    feature test user signup all passing

---
+++ app/helpers/attractionss_helper.rb
@@ -0,0 +1,2 @@
+module AttractionssHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :rides
+  resources :attractions
 
-
+  root 'static#welcome'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/signout' => 'sessions#destroy'
 end
